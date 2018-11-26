ae7076db1a0d57b4c63cfeeef405b95a88fa6b4f
Paul Wishart <misterwishart@gmail.com>
2018-11-26 23:26:10 +0000

    Tested file reading possibilities

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {console.log('JavaScript Loaded');})
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript Loaded');
+
+  const button = document.querySelector('#form')
+  button.addEventListener('submit', testFileFunction)
+})
+
+const testFileFunction = function (event) {
+  event.preventDefault();
+  const fileA = event.srcElement[0].files[0];
+  const reader = new FileReader();
+  reader.readAsText(fileA);
+
+  reader.addEventListener('loadend', (event) => {
+    const text = event.target.result;
+    const output = document.querySelector('#test-file');
+    output.textContent = text;
+  })
+}
