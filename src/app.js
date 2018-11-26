b34fafd28c97ef23b2d5a033c95ba3ba50374218
Paul Wishart <misterwishart@gmail.com>
2018-11-26 23:39:36 +0000

    Got two files reading

--- src/app.js
+++ src/app.js
@@ -7,13 +7,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const testFileFunction = function (event) {
   event.preventDefault();
-  const fileA = event.srcElement[0].files[0];
-  const reader = new FileReader();
-  reader.readAsText(fileA);
 
-  reader.addEventListener('loadend', (event) => {
+  const fileA = event.srcElement[0].files[0];
+  const fileB = event.srcElement[1].files[0];
+
+  console.dir(fileA);
+  console.dir(fileB);
+
+  const readerA = new FileReader();
+  readerA.readAsText(fileA);
+
+  const readerB = new FileReader();
+  readerB.readAsText(fileB);
+
+  readerA.addEventListener('loadend', (event) => {
     const text = event.target.result;
     const output = document.querySelector('#test-file');
-    output.textContent = text;
-  })
+    output.textContent += text;
+  });
+
+  readerB.addEventListener('loadend', (event) => {
+    const text = event.target.result;
+    const output = document.querySelector('#test-file');
+    output.textContent += text;
+  });
 }
